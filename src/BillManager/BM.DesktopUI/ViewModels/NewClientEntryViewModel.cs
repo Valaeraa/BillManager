73427c7adfb31f026f5bf3e40ddb1424a35bc16f
Valaeraa <valaeraa@gmail.com>
2020-06-06 17:23:52 +0300

    Wire Up NewClientEntry
    Wired up the NewClientViewModel to the data
    access layer and, finished implementing the logic.

--- src/BillManager/BM.DesktopUI/ViewModels/NewClientEntryViewModel.cs
+++ src/BillManager/BM.DesktopUI/ViewModels/NewClientEntryViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using BM.Library.DataAccess;
+using BM.Library.Models;
 using Stylet;
 using StyletIoC;
 using System;
@@ -12,15 +13,17 @@ namespace BM.DesktopUI.ViewModels
 {
     public class NewClientEntryViewModel : Screen
     {
-        private readonly IDefaultsData _data;
+        private readonly IWindowManager _windowManager;
+        private readonly IClientData _clientData;
+        private readonly IDefaultsData _defaultsData;
 
-        public NewClientEntryViewModel(IDefaultsData data)
+        public NewClientEntryViewModel(IWindowManager windowManager, IClientData clientData, IDefaultsData defaultsData)
         {
             DisplayName = "New Entry";
 
-            _data = data;
-
-            //LoadDefaultsFromDB();
+            _windowManager = windowManager;
+            _clientData = clientData;
+            _defaultsData = defaultsData;
         }
 
         protected override void OnActivate()
@@ -28,6 +31,30 @@ namespace BM.DesktopUI.ViewModels
             base.OnActivate();
 
             LoadDefaultsFromDB();
+        }
+
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set 
+            {
+                SetAndNotify(ref _name, value);
+                NotifyOfPropertyChange(() => CanAdd);
+            }
+        }
+
+        private string _email;
+
+        public string Email
+        {
+            get { return _email; }
+            set 
+            {
+                SetAndNotify(ref _email, value);
+                NotifyOfPropertyChange(() => CanAdd);
+            }
         }
 
         private string _hourlyRate;
@@ -38,7 +65,7 @@ namespace BM.DesktopUI.ViewModels
             set
             {
                 SetAndNotify(ref _hourlyRate, value);
-                NotifyOfPropertyChange(() => CanSubmit);
+                NotifyOfPropertyChange(() => CanAdd);
             }
         }
 
@@ -50,7 +77,7 @@ namespace BM.DesktopUI.ViewModels
             set
             {
                 SetAndNotify(ref _preBill, value);
-                NotifyOfPropertyChange(() => CanSubmit);
+                NotifyOfPropertyChange(() => CanAdd);
             }
         }
 
@@ -62,7 +89,7 @@ namespace BM.DesktopUI.ViewModels
             set
             {
                 SetAndNotify(ref _hasCutOff, value);
-                NotifyOfPropertyChange(() => CanSubmit);
+                NotifyOfPropertyChange(() => CanAdd);
             }
         }
 
@@ -74,7 +101,7 @@ namespace BM.DesktopUI.ViewModels
             set
             {
                 SetAndNotify(ref _cutOff, value);
-                NotifyOfPropertyChange(() => CanSubmit);
+                NotifyOfPropertyChange(() => CanAdd);
             }
         }
 
@@ -86,7 +113,7 @@ namespace BM.DesktopUI.ViewModels
             set
             {
                 SetAndNotify(ref _minimumHours, value);
-                NotifyOfPropertyChange(() => CanSubmit);
+                NotifyOfPropertyChange(() => CanAdd);
             }
         }
 
@@ -98,7 +125,7 @@ namespace BM.DesktopUI.ViewModels
             set
             {
                 SetAndNotify(ref _billingIncrement, value);
-                NotifyOfPropertyChange(() => CanSubmit);
+                NotifyOfPropertyChange(() => CanAdd);
             }
         }
 
@@ -110,14 +137,13 @@ namespace BM.DesktopUI.ViewModels
             set
             {
                 SetAndNotify(ref _roundUpAfterXMinutes, value);
-                NotifyOfPropertyChange(() => CanSubmit);
+                NotifyOfPropertyChange(() => CanAdd);
             }
         }
 
-
         private void LoadDefaultsFromDB()
         {
-            var model = _data.GetDefaultsData();
+            var model = _defaultsData.GetDefaultsData();
 
             if (model != null)
             {
@@ -131,18 +157,110 @@ namespace BM.DesktopUI.ViewModels
             }
         }
 
-        public bool CanSubmit
+
+
+        private ClientModel PopulateClientModel()
+        {
+            var output = new ClientModel
+            {
+                Name = Name,
+                Email = Email,
+                PreBill = PreBill ? 1 : 0,
+                HasCutOff = HasCutOff ? 1 : 0,
+                HourlyRate = double.Parse(HourlyRate),
+                CutOff = int.Parse(CutOff),
+                MinimumHours = double.Parse(MinimumHours),
+                BillingIncrement = double.Parse(BillingIncrement),
+                RoundUpAfterXMinutes = int.Parse(RoundUpAfterXMinutes)
+            };
+
+            return output;
+        }
+
+        private bool ValidateTextBoxes()
+        {
+            var output = true;
+
+            // Name
+            if (string.IsNullOrWhiteSpace(Name))
+            {
+                return false;
+            }
+
+            // Email
+            if (string.IsNullOrWhiteSpace(Email))
+            {
+                return false;
+            }
+
+            // HourlyRate
+            var isHourlyRateValid = double.TryParse(HourlyRate, out double hourlyRate);
+
+            if (isHourlyRateValid == false)
+            {
+                return false;
+            }
+
+            output = (hourlyRate < 0) ? false : true;
+
+            // CutOff
+            var isCutOffValid = int.TryParse(CutOff, out int cutOff);
+
+            if (isCutOffValid == false)
+            {
+                return false;
+            }
+
+            output = (cutOff < 0) ? false : true;
+
+            // MinimumHours
+            var isMinimumHoursValid = double.TryParse(MinimumHours, out double minimumHours);
+
+            if (isMinimumHoursValid == false)
+            {
+                return false;
+            }
+
+            output = (minimumHours < 0) ? false : true;
+
+            // BillingIncrement
+            var isBillingIncrementValid = double.TryParse(BillingIncrement, out double billingIncrement);
+
+            if (isBillingIncrementValid == false)
+            {
+                return false;
+            }
+
+            output = (billingIncrement < 0) ? false : true;
+
+            // RoundUpAfterXMinutes
+            var isRoundUpAfterXMinutesValid = int.TryParse(RoundUpAfterXMinutes, out int roundUpAfterXMinutes);
+
+            if (isRoundUpAfterXMinutesValid == false)
+            {
+                return false;
+            }
+
+            output = (roundUpAfterXMinutes < 0) ? false : true;
+
+            return output;
+        }
+
+        public bool CanAdd
         {
             get
             {
-                // TODO: Implement logic for submiting
-                return true;
+                return ValidateTextBoxes();
             }
         }
 
-        public void Submit()
+        public void Add()
         {
+            var model = PopulateClientModel();
 
+            _clientData.InsertClientData(model);
+
+            _windowManager.ShowMessageBox("The client has been added successfuly");
         }
 
         public void Cancel()
