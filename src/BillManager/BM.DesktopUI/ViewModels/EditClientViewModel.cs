d2e5681199320f07108d97da53c2b20fc7ca01fd
Valaeraa <valaeraa@gmail.com>
2020-06-06 17:19:49 +0300

    Added and bound all the necessary properties for EditClientViewModel.

--- src/BillManager/BM.DesktopUI/ViewModels/EditClientViewModel.cs
+++ src/BillManager/BM.DesktopUI/ViewModels/EditClientViewModel.cs
@@ -15,6 +15,14 @@ namespace BM.DesktopUI.ViewModels
             DisplayName = "Edit";
         }
 
+        private int _id;
+
+        public int Id
+        {
+            get { return _id; }
+            set { SetAndNotify(ref _id, value); }
+        }
+
         private string _name;
 
         public string Name
@@ -23,12 +31,86 @@ namespace BM.DesktopUI.ViewModels
             set { SetAndNotify(ref _name, value); }
         }
 
+        private string _email;
+
+        public string Email
+        {
+            get { return _email; }
+            set { SetAndNotify(ref _email, value); }
+        }
+
         private double _hourlyRate;
 
         public double HourlyRate
         {
             get { return _hourlyRate; }
             set { SetAndNotify(ref _hourlyRate, value); }
+        }
+
+        private bool _preBill;
+
+        public bool PreBill
+        {
+            get { return _preBill; }
+            set
+            {
+                SetAndNotify(ref _preBill, value);
+            }
+        }
+
+        private bool _hasCutOff;
+
+        public bool HasCutOff
+        {
+            get { return _hasCutOff; }
+            set
+            {
+                SetAndNotify(ref _hasCutOff, value);
+            }
+        }
+
+        private string _cutOff;
+
+        public string CutOff
+        {
+            get { return _cutOff; }
+            set
+            {
+                SetAndNotify(ref _cutOff, value);
+            }
+        }
+
+        private string _minimumHours;
+
+        public string MinimumHours
+        {
+            get { return _minimumHours; }
+            set
+            {
+                SetAndNotify(ref _minimumHours, value);
+            }
+        }
+
+        private string _billingIncrement;
+
+        public string BillingIncrement
+        {
+            get { return _billingIncrement; }
+            set
+            {
+                SetAndNotify(ref _billingIncrement, value);
+            }
+        }
+
+        private string _roundUpAfterXMinutes;
+
+        public string RoundUpAfterXMinutes
+        {
+            get { return _roundUpAfterXMinutes; }
+            set
+            {
+                SetAndNotify(ref _roundUpAfterXMinutes, value);
+            }
         }
 
         public void Cancel()
