0801fe2e3cec5e4e921b8fe8a910373db79f037d
Valaeraa <valaeraa@gmail.com>
2020-06-11 20:50:51 +0300

    Added Work data access

---
+++ src/BillManager/BM.Library/DataAccess/WorkData.cs
@@ -0,0 +1,34 @@
+﻿using BM.Library.Internal.DataAccess;
+using BM.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BM.Library.DataAccess
+{
+    public class WorkData
+    {
+        private const string _connectionString = "DefaultConnection";
+
+        public List<WorkModel> GetWorkData()
+        {
+            var sqlString = "select * from Work where ClientId = @ClientId";
+
+            var sql = new SqlDataAccess();
+            var output = sql.LoadData<WorkModel, dynamic>(sqlString, new { }, _connectionString);
+
+            return output;
+        }
+
+        public void UpdateWorkData(WorkModel parameters)
+        {
+            var sqlString = "update Work set Hours = @Hours, Title = @Title, Description = @Description, " +
+                "Paid = @Paid, PaymentId = @PaymentId where Id = @Id";
+
+            var sql = new SqlDataAccess();
+            sql.SaveData(sqlString, parameters, _connectionString);
+        }
+    }
+}
---
+++ src/BillManager/BM.Library/Models/WorkModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BM.Library.Models
+{
+    public class WorkModel
+    {
+        public int Id { get; set; }
+        public int ClientId { get; set; }
+        public double Hours { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string DateEntered { get; set; }
+        public int Paid { get; set; }
+        public int? PaymentId { get; set; }
+        public string DisplayValue
+        {
+            get
+            {
+                return $"{ DateEntered } - { Title }";
+            }
+        }
+    }
+}
